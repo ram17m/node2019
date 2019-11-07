cbbb02b3dc4b4918a25957b62169e66bcf988803
Ram babu Mishra <ramm.metropolia.fi>
2019-11-07 13:23:57 +0200

    express app, get / abd get / demo

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 'use strict';
-console.log('Hello wold');
+const express = require('express');
+const app = express();
+app.get('/',(req,res) =>{
+    res.send('Hello from the my Node server');
+
+});
+app.get('/demo',(req,res) =>{
+    console.log('request', req);
+    res.send('Demo');
+
+});
+app.listen(3000, () => {
+    console.log('Server app start?')
+});
+
