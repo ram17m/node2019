8dd9fdd191378fb639a3de8a5a403fa341093437
Ram babu Mishra <ramm.metropolia.fi>
2019-11-18 15:32:24 +0200

    SQL in separate module.exports

--- index.js
+++ index.js
@@ -1,54 +1,40 @@
 'use strict';
 
 const express = require('express');
-const connection = require('./model/db.js');
+const animal = require('./model/animal');
 
 const app = express();
-const bodyParser = require ('body-parser');
+const bodyParser = require('body-parser');
+
 app.use(express.static('public'));
 
-
 app.get('/animals', async (req, res) => {
-
+    // simple query
     try {
-        const [results, fields] = await connection.query(
-            'SELECT * FROM wild');
-
-        console.log(results); // results contains rows returned by server
-        console.log(fields); // fields contains extra meta data about results, if available
-        res.json(results);
+        res.json(await animal.getAll());
     } catch (e) {
         console.log(e);
-        res.send('db error...');
+        res.send('db error :(');
     }
-
 });
 
-app.get('/animal' ,async(req, res) => {
+app.get('/animal', async (req, res) => {
     console.log(req.query);
-   // res.send(`query params? ${req.query}`);
-   try {
-       const [results] = await connection.query(
-           'SELECT * FROM wild WHERE name LIKE ?',
-           [req.query.name]);
-        res.json(results);
-   } catch(e) {
-       res.send(`db error ${e}`);
-   }
-
+    try {
+        res.json(await animal.search(req.query.name));
+    } catch (e) {
+        res.send('db error');
+    }
 });
 
-app.post('/animal',bodyParser.urlencoded({extended: true}), async (req,res) => {
+app.post('/animal', bodyParser.urlencoded({extended: true}), async (req, res) => {
     console.log(req.body);
-     try {
-         const [result] = await connection.query('INSERT INTO wild (name) VALUES (?)',
-         [req.body.name]
-         );
-        res.json(result);
-
-     } catch (e){
-         console.log(e);
-     }
+    try {
+        res.json(await animal.insert(req.body.name, req.body.dob));
+    } catch (e) {
+        console.log(e);
+        res.send('db error');
+    }
 });
 
 app.get('/', (req, res) => {
---
+++ model/animal.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const connection = require('./db');
+
+exports.getAll = async () => {
+    try {
+        const [results, fields] = await connection.query('SELECT * FROM animal');
+        console.log(results); // results contains rows returned by server
+        console.log(fields); // fields contains extra meta data about results, if available
+        return results;
+    } catch (e) {
+        console.log(e);
+        throw 'db error :(';
+    }
+};
+
+exports.search = async (name) => {
+    try {
+        const [results] = await connection.query(
+            'SELECT * FROM wild WHERE name LIKE ?',
+            [name]); 
+        return results;
+    } catch (e) {
+        console.log(e);
+        throw `db error`;
+    }
+};
+
+exports.insert = async (name, dob) => {
+    try {
+        const [results] = await connection.query(
+            'INSERT INTO wild (name, date_of_birth) VALUES (?, ?)',
+            [name, dob]); 
+        return results;
+    } catch (e) {
+        console.log(e);
+        throw 'db error';
+    }
+};
